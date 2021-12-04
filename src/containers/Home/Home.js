547d5e3eea148ce19a5720b995822c34db0cb152
Ollie Franklin <ollie.franklin3@gmail.com>
2021-12-04 18:54:41 +1300

    Add stars to home page

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -2,10 +2,49 @@ import React from 'react';
 import logo from '../../assets/logo.svg';
 import planet1 from '../../assets/planet1.svg';
 import planet2 from '../../assets/planet2.svg';
+import star1 from '../../assets/star1.svg';
+import star2 from '../../assets/star2.svg';
 import './Home.css';
 
 export const Home = () => {
   const [isShowingProjectDesc, setIsShowingProjectDesc] = React.useState(false);
+
+  const [starElements, setStarElements] = React.useState([]);
+
+  React.useEffect(() => {
+    const getRandomStar = (source, key) => {
+      let left = Math.random();
+      let top = Math.random();
+
+      const styles = {
+        animationDuration: `${5 + Math.random() * 10}s`,
+        animationDelay: `${10 * (0.5 - Math.random())}s`,
+        left: `${left * 100}%`,
+        top: `${top * 100}%`,
+        height: '2%',
+      };
+
+      return (
+        <img src={source} style={styles} className="star" key={key} alt={key} />
+      );
+    };
+
+    const screenWidth =
+      window.innerWidth || document.documentElement.clientWidth;
+    const screenHeight =
+      window.innerHeight || document.documentElement.clientHeight;
+    const numStars = (screenWidth * screenHeight) / 25000;
+
+    const stars = [];
+
+    for (let i = 0; i < numStars; i++) {
+      const source = i % 2 ? star1 : star2;
+      const key = `star${i}`;
+      stars.push(getRandomStar(source, key));
+    }
+
+    setStarElements(stars);
+  }, [setStarElements]);
 
   const MainContainer = () => {
     return (
@@ -85,6 +124,8 @@ export const Home = () => {
     <div className="background">
       <img src={planet1} className="planet planet1" alt="Planet 1" />
       <img src={planet2} className="planet planet2" alt="Planet 2" />
+
+      {starElements}
 
       <MainContainer />
 
