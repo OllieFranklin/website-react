671190814e7ef0c4a28afd45a7c3212b9266ed48
Ollie Franklin <ollie.franklin3@gmail.com>
2022-01-08 12:20:29 +1300

    Create stats component and implement next box

--- src/containers/Tetris/view/GameBoard.jsx
+++ src/containers/Tetris/view/GameBoard.jsx
@@ -2,15 +2,17 @@ import React from 'react';
 
 import { useTetrisController } from '../controller';
 import { tetrominoTexturesDefault } from '../../../assets/Tetris/tetrominoes';
+import { Stats } from './Stats';
 
 export const GameBoard = () => {
   const [boardHasLoaded, setBoardHasLoaded] = React.useState(false);
   const [canvasStyles, setCanvasStyles] = React.useState({});
+  const [cellSize, setCellSize] = React.useState(0);
 
   const canvasRef = React.useRef();
   const pageRef = React.useRef();
 
-  const { board, stats, startGame } = useTetrisController();
+  const { board, startGame, stats } = useTetrisController();
 
   // resize function for board
   React.useEffect(() => {
@@ -36,6 +38,8 @@ export const GameBoard = () => {
       // // also update the stats container to be the same height as the board
       // document.querySelector('#stats-container').style.height =
       //   this.canvas.style.height;
+
+      setCellSize(canvasRef.current.width / 10);
 
       setBoardHasLoaded(true);
     };
@@ -64,16 +68,14 @@ export const GameBoard = () => {
       const rows = board.current.length;
       const cols = board.current[0].length;
 
-      const cellSize = canvas.width / cols;
-
       for (let row = 0; row < rows; row++) {
         const y = row * cellSize;
 
         for (let col = 0; col < cols; col++) {
           const x = col * cellSize;
 
-          const tetromino = board.current[row][col];
-          const texture = tetrominoTexturesDefault[tetromino];
+          const cell = board.current[row][col];
+          const texture = tetrominoTexturesDefault[cell];
 
           if (texture) {
             ctx.drawImage(texture, x, y, cellSize, cellSize);
@@ -88,7 +90,7 @@ export const GameBoard = () => {
 
     // ignore startGame dependency
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [board]);
+  }, [board, cellSize]);
 
   return (
     <div className="align-items-center h-100" id="game-container" ref={pageRef}>
@@ -100,85 +102,7 @@ export const GameBoard = () => {
           ref={canvasRef}
         ></canvas>
 
-        {boardHasLoaded && (
-          <div id="stats-container" className="d-none d-md-flex">
-            <div className="row my-card shadow-sm" id="stats-panel-1">
-              <div style={{ height: '20%' }}>
-                <h4>
-                  <b>Next Piece</b>
-                </h4>
-              </div>
-              <canvas id="next-box"></canvas>
-            </div>
-            <div className="row my-card shadow-sm" id="stats-panel-2">
-              <div style={{ height: '33%' }}>
-                <h4>
-                  <b>Level</b>
-                  <span className="stat-sm">
-                    <span>&nbsp;–&nbsp;&nbsp;</span>
-                    <span className="level-stat">{stats.level}</span>
-                  </span>
-                </h4>
-                <h4 className="stat-lg level-stat">{stats.level}</h4>
-              </div>
-              <div style={{ height: '33%' }}>
-                <h4>
-                  <b>Lines</b>
-                  <span className="stat-sm">
-                    <span>&nbsp;–&nbsp;&nbsp;</span>
-                    <span className="lines-stat">{stats.lines}</span>
-                  </span>
-                </h4>
-                <h4 className="stat-lg lines-stat">{stats.lines}</h4>
-              </div>
-              <div style={{ height: '33%' }}>
-                <h4>
-                  <b>Score</b>
-                  <span className="stat-sm">
-                    <span>&nbsp;–&nbsp;&nbsp;</span>
-                    <span className="score-stat">{stats.score}</span>
-                  </span>
-                </h4>
-                <h4 className="stat-lg score-stat">{stats.score}</h4>
-              </div>
-            </div>
-            <div className="row my-card shadow-sm" id="stats-panel-3">
-              <div style={{ height: '33%' }}>
-                <h4>
-                  <b>Tetris Rate</b>
-                  <span className="stat-sm">
-                    <span>&nbsp;–&nbsp;&nbsp;</span>
-                    <span className="tetris-rate-stat">{stats.tetrisRate}</span>
-                    %
-                  </span>
-                </h4>
-                <h4 className="stat-lg">
-                  <span className="tetris-rate-stat">{stats.tetrisRate}</span>%
-                </h4>
-              </div>
-              <div style={{ height: '33%' }}>
-                <h4>
-                  <b>Drought</b>
-                  <span className="stat-sm">
-                    <span>&nbsp;–&nbsp;&nbsp;</span>
-                    <span className="drought-stat">{stats.drought}</span>
-                  </span>
-                </h4>
-                <h4 className="stat-lg drought-stat">{stats.drought}</h4>
-              </div>
-              <div style={{ height: '33%' }}>
-                <h4>
-                  <b>Burn</b>
-                  <span className="stat-sm">
-                    <span>&nbsp;–&nbsp;&nbsp;</span>
-                    <span className="burn-stat">{stats.burn}</span>
-                  </span>
-                </h4>
-                <h4 className="stat-lg burn-stat">{stats.burn}</h4>
-              </div>
-            </div>
-          </div>
-        )}
+        {boardHasLoaded && <Stats stats={stats} cellSize={cellSize} />}
       </div>
     </div>
   );
---
+++ src/containers/Tetris/view/Stats.jsx
@@ -0,0 +1,97 @@
+import React from 'react';
+import { tetrominoTexturesDefault } from '../../../assets/Tetris/tetrominoes';
+
+const StatsItem = ({ name, value }) => {
+  return (
+    <div style={{ height: '33%' }}>
+      <h4>
+        <b>{name}</b>
+        <span className="stat-sm">&nbsp;–&nbsp;&nbsp;{value || 0}</span>
+      </h4>
+      <h4 className="stat-lg">{value || 0}</h4>
+    </div>
+  );
+};
+
+export const Stats = ({ stats, cellSize }) => {
+  const { nextPiece, level, lines, score, tetrisRate, drought, burn } = stats;
+
+  const canvasRef = React.useRef();
+
+  // resize function for next box
+  React.useEffect(() => {
+    const handleResize = () => {
+      // and make sure that the width & height of the canvas match the style width & height
+      if (canvasRef.current.height !== canvasRef.current.offsetHeight) {
+        canvasRef.current.height = canvasRef.current.offsetHeight;
+        canvasRef.current.width = canvasRef.current.offsetWidth;
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    setTimeout(handleResize, 10);
+  }, []);
+
+  React.useEffect(() => {
+    const renderNextBox = () => {
+      const canvas = canvasRef.current;
+      const ctx = canvas.getContext('2d');
+
+      if (!nextPiece) {
+        return;
+      }
+
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+      const tetromino = nextPiece;
+      const rows = tetromino.length;
+      const cols = tetromino[0].length;
+
+      const left = Math.round((canvas.width - cols * cellSize) * 0.5);
+      const top = Math.round((canvas.height - rows * cellSize) * 0.5);
+
+      for (let row = 0; row < rows; row++) {
+        const y = top + row * cellSize;
+
+        for (let col = 0; col < cols; col++) {
+          const x = left + col * cellSize;
+
+          const cell = tetromino[row][col];
+
+          const texture = tetrominoTexturesDefault[cell];
+          if (texture) {
+            ctx.drawImage(texture, x, y, cellSize, cellSize);
+          }
+        }
+      }
+    };
+
+    requestAnimationFrame(renderNextBox);
+  }, [nextPiece, cellSize]);
+
+  return (
+    <div id="stats-container" className="d-md-flex">
+      <div className="row my-card shadow-sm" id="stats-panel-1">
+        <div style={{ height: '20%' }}>
+          <h4>
+            <b>Next Piece</b>
+          </h4>
+        </div>
+        <canvas id="next-box" ref={canvasRef}></canvas>
+      </div>
+
+      <div className="row my-card shadow-sm" id="stats-panel-2">
+        <StatsItem name="Level" value={level} />
+        <StatsItem name="Lines" value={lines} />
+        <StatsItem name="Score" value={score} />
+      </div>
+
+      <div className="row my-card shadow-sm" id="stats-panel-3">
+        <StatsItem name="Tetris Rate" value={`${tetrisRate}%`} />
+        <StatsItem name="Drought" value={drought} />
+        <StatsItem name="Burn" value={burn} />
+      </div>
+    </div>
+  );
+};
