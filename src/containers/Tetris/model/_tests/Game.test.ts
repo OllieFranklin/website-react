d8382891772b4d3a6c9c09540c59a2d1e57c802d
Ollie Franklin <ollie.franklin3@gmail.com>
2022-01-09 20:04:50 +1300

    Fully implement oracle comparison test with custom matcher

--- src/containers/Tetris/model/_tests/Game.test.ts
+++ src/containers/Tetris/model/_tests/Game.test.ts
@@ -7,17 +7,78 @@ function gameStateBoardToJsonFileBoard(board: string[][]): string {
     .join('');
 }
 
-test('First frames are the same', () => {
+function getCustomFailureMessage(
+  expected: string,
+  recieved: string,
+  { frameNum }: info,
+): string {
+  const boardToPrintableArray = (board: string): string[] => {
+    return board.replaceAll('0', '░░').replaceAll('1', '██').split('\n');
+  };
+
+  let errorString = `On frame ${frameNum}\n\nExpected board         Recieved board\n`;
+  const expectedArray = boardToPrintableArray(expected);
+  const recievedArray = boardToPrintableArray(recieved);
+  for (let i = 0; i < expectedArray.length; i++) {
+    let expectedRow = expectedArray[i];
+    let recievedRow = recievedArray[i];
+
+    if (expectedRow !== recievedRow) {
+      recievedRow = recievedRow.replaceAll('░', '▒');
+    }
+
+    errorString += `${expectedRow}   ${recievedRow}\n`;
+  }
+
+  return errorString;
+}
+
+type info = { frameNum: number };
+
+declare global {
+  namespace jest {
+    interface Matchers<R> {
+      toMatchBoard(recieved: string, info: info): R;
+    }
+  }
+}
+
+expect.extend({
+  toMatchBoard(expected: string, received: string, info: info) {
+    const pass = expected === received;
+    if (pass) {
+      return {
+        message: () => '',
+        pass: true,
+      };
+    } else {
+      return {
+        message: () => getCustomFailureMessage(expected, received, info),
+        pass: false,
+      };
+    }
+  },
+});
+
+test('Model matches test oracle', () => {
   const { pieceOrder, startingLevel, frames } = testOutput;
 
   const game = new Game(startingLevel, pieceOrder);
 
-  const frame = frames[0];
+  let recievedFrameNum = 0;
 
-  const { keyState, boardState: expectedBoard } = frame;
+  for (let i = 0; i < frames.length; i++) {
+    const { boardState: expectedBoard, keyState } = frames[i];
+    const { frameNum: nextExpectedFrameNum = -1 } = frames[i + 1] || {};
 
-  const gameState = game.nextFrame(keyState);
-  const actualBoard = gameStateBoardToJsonFileBoard(gameState.board);
+    while (recievedFrameNum < nextExpectedFrameNum) {
+      const gameState = game.nextFrame(keyState);
+      const recievedBoard = gameStateBoardToJsonFileBoard(gameState.board);
 
-  expect(actualBoard).toBe(expectedBoard);
+      const info = { frameNum: recievedFrameNum };
+      expect(expectedBoard).toMatchBoard(recievedBoard, info);
+
+      recievedFrameNum++;
+    }
+  }
 });
