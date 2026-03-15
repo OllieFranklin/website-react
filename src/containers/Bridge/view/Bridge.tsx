2c8796744da5e556cf901f03e0f3bfec936183a7
Ollie Franklin <ollie.franklin3@gmail.com>
2026-03-15 17:41:22 +1300

    Add back button

--- src/containers/Bridge/view/Bridge.tsx
+++ src/containers/Bridge/view/Bridge.tsx
@@ -3,6 +3,8 @@ import Stack from '@mui/material/Stack';
 import IconButton from '@mui/material/IconButton';
 import RefreshIcon from '@mui/icons-material/Refresh';
 import SettingsIcon from '@mui/icons-material/Settings';
+import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
+import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 import { Contract } from '../model/constants';
 import {
   DEFAULT_RANDOM_CONTRACT_OPTIONS,
@@ -28,7 +30,34 @@ const Bridge: React.FC<BridgeProps> = props => {
     () => ({ contract: randomContract(contractOptions), showFront: true }),
     [contractOptions],
   );
-  const [card, setCard] = React.useState<CardData>(randomCard());
+
+  const [cardHistory, setCardHistory] = React.useState([randomCard()]);
+  const [cardIndex, setCardIndex] = React.useState(0);
+  const card = cardHistory.at(cardIndex) ?? cardHistory[cardIndex - 1];
+
+  const nextCard = React.useCallback(() => {
+    setCardIndex(currentIndex => {
+      if (currentIndex < cardHistory.length - 1) {
+        return currentIndex + 1;
+      }
+
+      setCardHistory(history => [...history, randomCard()]);
+      return currentIndex + 1;
+    });
+  }, [cardHistory.length, randomCard, setCardIndex, setCardHistory]);
+
+  const prevCard = React.useCallback(() => {
+    setCardIndex(currentIndex => Math.max(currentIndex - 1, 0));
+  }, [setCardIndex]);
+
+  const handleFlip = React.useCallback(() => {
+    setCardHistory(hist => {
+      const newCardHistory = [...hist];
+      const newCard: CardData = { ...card, showFront: !card.showFront };
+      newCardHistory.splice(cardIndex, 1, newCard);
+      return newCardHistory;
+    });
+  }, [card, setCardHistory]);
 
   return (
     <Stack
@@ -58,11 +87,18 @@ const Bridge: React.FC<BridgeProps> = props => {
           key={`${JSON.stringify(card.contract)}`}
           contract={card.contract}
           showFront={card.showFront}
-          handleFlip={() => setCard(c => ({ ...c, showFront: !c.showFront }))}
+          handleFlip={handleFlip}
         />
-        <Stack direction="row" sx={{ justifyContent: 'flex-end' }}>
-          <IconButton color="default" onClick={() => setCard(randomCard())}>
-            <RefreshIcon />
+        <Stack direction="row" sx={{ justifyContent: 'space-between' }}>
+          <IconButton
+            color="default"
+            onClick={() => prevCard()}
+            disabled={cardIndex === 0}
+          >
+            <NavigateBeforeIcon />
+          </IconButton>
+          <IconButton color="default" onClick={() => nextCard()}>
+            <NavigateNextIcon />
           </IconButton>
         </Stack>
       </Stack>
